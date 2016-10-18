356665afd8124c5d9916e503f4fef56bb58428e5
Erik E <shamrice@gmail.com>
2016-10-18 15:07:46 -0400

    Ticket #18 - Added TedPollingAPI.Stats to Export
    TedPollingAPI Stats are now available for export using the data
    exporter.

--- PowerMonitoringUtility/DataExporter/Core/Exporter.cs
+++ PowerMonitoringUtility/DataExporter/Core/Exporter.cs
@@ -117,11 +117,70 @@ namespace DataExporter.Core
                 if (null != tedStats)
                 {
                     string endOfFileName = "Stats.csv";
+
+                    //write header if new file.
                     bool fileExists = File.Exists(this.fileLocation + "\\" + this.baseFileName + endOfFileName);
                     if (!fileExists)
-                        WriteToFile(endOfFileName, csvBuilder.Build());
+                    {
+                        string header = "Date,";
+                        foreach (var mtu in tedStats.MtuVal.Mtus)
+                        {
+                            string mtuPrefix = "MTU" + mtu.MtuNumber + "_";
+                            header += csvBuilder.Build(mtuPrefix + "PowerNow", mtuPrefix + "VoltageNow",
+                                mtuPrefix + "KvaNow", mtuPrefix + "MtuRec", mtuPrefix + "MtuSkp", mtuPrefix + "MinCount",
+                                 mtuPrefix + "ManCalPower", mtuPrefix + "ManCalVoltage", mtuPrefix + "MtuId",
+                                  mtuPrefix + "MtuVer", mtuPrefix + "Timestamp", mtuPrefix + "MtuCumulative",
+                                   mtuPrefix + "PfAvg", mtuPrefix + "Uptime", mtuPrefix + "PhaseType", mtuPrefix + "Modulation",
+                                    mtuPrefix + "SQ", mtuPrefix + "PhaseCurrent.A", mtuPrefix + "PhaseCurrent.B",
+                                     mtuPrefix + "PhaseCurrent.C", mtuPrefix + "PhaseVoltage.A", mtuPrefix + "PhaseVoltage.B",
+                                      mtuPrefix + "PhaseVoltage.C", mtuPrefix + "UseTcp");
+                        }
+                        int thirdPartyNum = 1;
+                        foreach (var thirdParty in tedStats.ThirdPartyPosting.ThirdParty)
+                        {
+                            string thirdPartyPrefix = "ThirdParty" + thirdPartyNum + "_";
 
-                    WriteToFile(endOfFileName, csvBuilder.Build());
+                            header += csvBuilder.Build(thirdPartyPrefix + "Activated", thirdPartyPrefix + "ActStatus", thirdPartyPrefix + "Attempts"
+                            , thirdPartyPrefix + "Success", thirdPartyPrefix + "Results", thirdPartyPrefix + "TimeStamp"
+                            , thirdPartyPrefix + "Host", thirdPartyPrefix + "Port", thirdPartyPrefix + "URI");
+
+                            thirdPartyNum++;
+                        }
+
+                        header += csvBuilder.Build("ThirdParty_Date", "ThirdParty_Uptime", "ThirdParty_LastMTUID");
+
+                        header += csvBuilder.Build("Bootloader_Uploaded", "Bootloader_Sent");
+                        header += csvBuilder.Build("USB_Mode", "USB_Status", "USB_LastError", "USB_ErrorCount", "USB_RxCount", "USB_TxCount");
+                        
+                        WriteToFile(endOfFileName, header);
+                    }
+
+                    //write data.
+                    string data = DateTime.Now.ToString() + ",";
+                    foreach (var mtu in tedStats.MtuVal.Mtus)
+                    {
+                        data += csvBuilder.Build(mtu.PowerNow, mtu.VoltageNow,
+                                mtu.KvaNow, mtu.MtuRec, mtu.MtuSkp, mtu.MinCount,
+                                 mtu.ManCalPower, mtu.ManCalVoltage, mtu.MtuId,
+                                  mtu.MtuVer, mtu.Timestamp, mtu.MtuCumulative,
+                                   mtu.PfAvg, mtu.Uptime, mtu.PhaseType, mtu.Modulation,
+                                    mtu.SQ, mtu.PhaseCurrent.A, mtu.PhaseCurrent.B,
+                                     mtu.PhaseCurrent.C, mtu.PhaseVoltage.A, mtu.PhaseVoltage.B,
+                                      mtu.PhaseVoltage.C, mtu.UseTcp);                        
+                    }
+
+                    foreach (var thirdParty in tedStats.ThirdPartyPosting.ThirdParty)
+                    {
+                        data += csvBuilder.Build(thirdParty.Activated,thirdParty.ActStatus,thirdParty.Attempts,thirdParty.Success,
+                            thirdParty.Results,thirdParty.TimeStamp,thirdParty.Host,thirdParty.Port,thirdParty.Uri);
+                    }
+
+                    data += csvBuilder.Build("NULL", "NULL", "NULL");
+                    data += csvBuilder.Build(tedStats.Bootloader.Uploaded, tedStats.Bootloader.Sent);
+                    data += csvBuilder.Build(tedStats.Usb.Mode, tedStats.Usb.Status, tedStats.Usb.LastError, tedStats.Usb.ErrorCount
+                        , tedStats.Usb.RxCount, tedStats.Usb.TxCount);
+
+                    WriteToFile(endOfFileName, data);
                 }
             }
         }
